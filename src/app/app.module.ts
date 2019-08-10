0dc337fc1fdcec52e84bb2c8254b0417b51f3f6f
Lakshyajit <lakshyajit165@gmail.com>
2019-08-10 12:13:41 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SignupPipe } from './signup.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SignupPipe],
   entryComponents: [],
   imports: [
     BrowserModule,
