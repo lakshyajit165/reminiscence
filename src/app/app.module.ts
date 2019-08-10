06f28e39341b811c56f6787da36ca37a63c6c0d6
Lakshyajit <lakshyajit165@gmail.com>
2019-08-10 19:17:50 +0530

    Firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SignupPipe } from './signup.pipe';
+
+import * as firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyD2adlxVujPys9MBc0I4Ugrk4uD3iYa0t8',
+  authDomain: 'reminiscence-70637.firebaseapp.com',
+  databaseURL: 'https://reminiscence-70637.firebaseio.com',
+  projectId: 'reminiscence-70637',
+  storageBucket: '',
+  messagingSenderId: '176285555752',
+  appId: '1:176285555752:web:68dd8d7fa40a940b'
+});
+
 
 @NgModule({
-  declarations: [AppComponent, SignupPipe],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
